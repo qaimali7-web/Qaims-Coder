aa3d7f4a0ed7a42a7675b77e2db17230d17e1914
Qaim Ali <qaimali7@gmail.com>
2026-03-19 19:46:08 +0500

    error fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
-import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
-    },
-  },
+  plugins: [react()],
   server: {
     hmr: process.env.DISABLE_HMR !== 'true',
   },
